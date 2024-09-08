b85eb4f92b1a85cf2efe239e0406aa8c462972b0
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-09-08 16:59:39 +0300

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
 import Appointments from "./pages/Appointments";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 export default function App() {
@@ -27,7 +28,9 @@ export default function App() {
       <Route path="/my-profile" element={<MyProfile/>} />
       <Route path="/my-appointments" element={<MyAppointments/>} />
       <Route path="/appointments/:docId" element={<Appointments/>} />
+      
      </Routes>
+     <Footer />
 
     </div>
   )
--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
         </div>
         <button onClick={()=> {navigate('/login'); scrollTo(0,0 )}}  className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all '>Create account</button>
       </div>
- text
+ 
       {/*----------right side---------- */}
       <div className='hidden md:block md:w-1/2 lg:w-[380px] relative'>
         <img className='w-full absolute bottom-0 right-0 max-w-md ' src={assets.appointment_img} alt="" />

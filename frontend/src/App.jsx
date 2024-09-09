65148f37df271aabae72a55c28262c673dc0498a
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-09-09 14:12:05 +0300

    created top part of appointment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,9 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
-import Appointments from "./pages/Appointments";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Appointment from "./pages/Appointment";
 
 
 export default function App() {
@@ -27,7 +27,7 @@ export default function App() {
       <Route path="/contact" element={<Contact/>} />
       <Route path="/my-profile" element={<MyProfile/>} />
       <Route path="/my-appointments" element={<MyAppointments/>} />
-      <Route path="/appointments/:docId" element={<Appointments/>} />
+      <Route path="/appointment/:docId" element={<Appointment />} />
       
      </Routes>
      <Footer />

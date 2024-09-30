8f168b4c2ac2fe3e8705a81d29191ffb849c79cd
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-09-30 22:47:37 +0300

    added user model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 
 //app config
@@ -10,6 +11,8 @@ const port = process.env.PORT || 5000;
 
 //db connection
 connectDB();
+connectCloudinary();
+
 
 // middlewares
 app.use(express.json());

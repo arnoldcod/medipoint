133c788ea525774b47e95b6ce9be11c76961341a
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-10-07 00:00:20 +0300

    added getallDoctors function in admincontroller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -83,4 +83,15 @@ const loginAdmin = async (req, res) => {
     }
 };
 
-export { addDoctor, loginAdmin }
+//API to get all doctors list for admin panel
+const getAllDoctors = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({}).select('-password')
+        res.json({ success: true, doctors });
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: 'Server Error' });
+    }
+};
+
+export { addDoctor, loginAdmin, getAllDoctors }
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import upload from '../middlewares/multer.js'
-import { addDoctor, loginAdmin } from '../controllers/adminController.js';
+import { addDoctor, getAllDoctors, loginAdmin } from '../controllers/adminController.js';
 import authAdmin from '../middlewares/authAdmin.js';
 
 
@@ -9,5 +9,6 @@ const adminRouter = express.Router();
 
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/login', loginAdmin)
+adminRouter.post('/all-doctors', authAdmin ,getAllDoctors)
 
 export default adminRouter;

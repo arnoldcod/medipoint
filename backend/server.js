9c10c6d8650665b0561d605475d907cc5406f350
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-10-07 20:30:32 +0300

    added userController annd userRoute for user register api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
 
 
 //app config
@@ -23,6 +24,7 @@ app.use(cors());
 // routes 
 app.use('/api/admin', adminRouter) //localhost:5000/api/admin/add-doctor
 app.use('/api/doctor', doctorRouter) 
+app.use('/api/user', userRouter )
 
 
 

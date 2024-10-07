ca6dcc93f2e8429e87a230b63d4f93ca7b29550a
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-10-07 22:38:40 +0300

    addedloadUserProfileData functionality in My profile

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,8 +9,10 @@ const AppContextProvider =(props)=> {
 
     const currencySymbol = "$"
     const backendURL = import.meta.env.VITE_BACKEND_URL
+
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false )
+    const [userData, setUserData] = useState(false)
 
     
 
@@ -28,17 +30,46 @@ const AppContextProvider =(props)=> {
         }
     }
 
+    
+    const loadUserProfileData = async ()=> {
+        try {
+            const {data} = await axios.get(backendURL + '/api/user/get-profile', {headers:{token}})
+
+            if(data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+
     const value = {
         doctors, currencySymbol,
         token,
         setToken, 
-        backendURL
+        backendURL,
+        userData,
+        setUserData,
+        loadUserProfileData
      }
 
      
     useEffect(()=> {
       getDoctorsData()
     },[])
+
+    useEffect(()=> {
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    },[token])
 
     return (
         <AppContext.Provider value={value}>
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,24 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const MyProfile = () => {
-    const [userData, setUserData] = useState({
-        name: "Edward Vincent",
-        image: assets.profile_pic,
-        email: "edwardvincent@gmail.com",
-        phone: "1234567890",
-        address: {
-            line1: "57th Cross, Richmond",
-            line2: "Circle, Ring Road, London",
-        },
-        gender: "Male",
-        dob: "2000-01-20",
-    });
+
+    const {userData, setUserData} = useContext(AppContext)
 
     const [isEdit, setIsEdit] = useState(false);
 
-    return (
+    return userData && (
         <div className="max-w-lg flex flex-col gap-2 text-sm">
             <img className="w-36 rounded" src={userData.image} alt="" />
 
